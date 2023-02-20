a76da5155a20346f39fb50a247c9dc1891752af8
ffran.acosta <francoaugustoacosta@gmail.com>
2023-02-20 08:47:39 -0300

    people table - atomic design

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,84 +1,18 @@
 import { People } from "@/data";
-import { Person } from "@/models";
-import { addFavorite } from "@/redux/states/favorite";
 import { addPeople } from "@/redux/states/people";
-import store, { AppStore } from "@/redux/store";
-import { Checkbox } from "@mui/material";
-import { DataGrid, GridRenderCellParams } from "@mui/x-data-grid"
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from 'react-redux';
+import { PeopleTable } from "../components";
 
 const Home = () => {
 
-    //FOR CHECKBOX, Trabaja sonre el moedelo de <Person>
-    const [selectedPeople, setSelectedPeople] = useState<Person[]>([])
-
-    const statePeople = useSelector((store: AppStore) => store.people);
-    const favoritePeople = useSelector((store: AppStore) => store.favorites);
-    //Find people and 
-    const findPerson = (person: Person) => !!favoritePeople.find(p => p.id === person.id);
-    const filterPerson = (person: Person) => favoritePeople.filter(p => p.id !== person.id);
-
     const dispatch = useDispatch();
-    const handleChange = (person: Person) => {
-        const filteredPeople = findPerson(person) ? filterPerson(person) : [...selectedPeople, person];
-        dispatch(addFavorite(filteredPeople));
-        setSelectedPeople(filteredPeople);
-    };
-    
-    // COLUMNS CONFIGURATION
-    const colums = [
-        {
-            field: 'actions',
-            type: 'actions',
-            sortable: false,
-            headerName: '',
-            width: 50,
-            renderCell: (params: GridRenderCellParams) => (
-                <>{<Checkbox size="small" checked={findPerson(params.row)} onChange={() => handleChange(params.row)} />}</>
-            )
-        },
-        {
-            field: 'name',
-            headerName: 'Name',
-            flex: 1,
-            minWidth: 150,
-        },
-        {
-            field: 'category',
-            headerName: 'Categories',
-            flex: 1,
-        },
-        {
-            field: 'company',
-            headerName: 'Company',
-            flex: 1,
-        },
-        {
-            field: 'levelOfHappiness',
-            headerName: 'Level of happiness',
-            flex: 1,
-        }
-    ];
-
     useEffect(() => {
-        dispatch(addPeople(People))
+        dispatch(addPeople(People));
     }, [])
 
-    // PAGE GRID CONFIG (5 PEOPLE)
-    const pageSize = 5;
-
     return (
-        <DataGrid
-            rows={store.getState().people}
-            columns={colums}
-            disableColumnSelector
-            disableSelectionOnClick
-            autoHeight
-            pageSize={pageSize}
-            rowsPerPageOptions={[pageSize]}
-            getRowId={(row: any) => row.id}
-        />
+        <PeopleTable/>
     )
 }
-export default Home
+export default Home;
